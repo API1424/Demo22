5e739f3a601e8411b0c5f57fa4e7616a5d41e01c
API1424 <88828934+API1424@users.noreply.github.com>
2021-08-12 15:56:08 +0530

    3rd commit

--- Sample.java
+++ Sample.java
@@ -1,7 +1,8 @@
 class Sample{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
-
-System.out.println("Hello Rohit"); 
+    System.out.println("Hello Rohit"); 
+    System.out.println("Hello part 2"); 
+    System.out.println("Hello part 3");    
     }  
 }  
